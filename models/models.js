92290e09509dbe4cf3a17d09083bf0d69a2ac325
antonio-san-agustin <antonio.sanagustin.garcia@alumnos.upm.e>
2015-05-09 11:42:36 +0200

    Despliegue en Heroku

--- models/models.js
+++ models/models.js
@@ -1,10 +1,7 @@
-
 var path = require('path');
-
 
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite DATABASE_URL = sqlite://:@:/
-
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name = (url[6]||null);
 var user = (url[2]||null);
@@ -20,7 +17,6 @@ var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd,
-
 { dialect: protocol,
 protocol: protocol,
 port: port,
@@ -30,21 +26,24 @@ omitNull: true // solo Postgres
 }
 );
 
+
+// Importar definicion de la tabla Quiz
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
-exports.Quiz = Quiz;
+exports.Quiz = Quiz; // exportar tabla Quiz
 
+// sequelize.sync() inicializa tabla de preguntas en DB
 
-sequelize.sync().success(function(){
+sequelize.sync().then(function() {
 
-
-Quiz.count().success (function (count){
-            if(count === 0){
-                  Quiz.create ({ pregunta: 'Capital de Italia',
-                                                 respuesta: 'Roma'
-                             })
-                    .success (function(){console.log('Base de datos inicializada')});
-                  };
-             });
-        });
+// then(..) ejecuta el manejador una vez creada la tabla
+Quiz.count().then(function (count){
+if(count === 0) { // la tabla se inicializa solo si está vacía
+Quiz.create({ pregunta: 'Capital de Italia',
+respuesta: 'Roma'
+})
+.then(function(){console.log('Base de datos inicializada')});
+};
+});
+});
